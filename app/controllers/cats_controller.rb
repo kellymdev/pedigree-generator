4fcd24d75800fd39eb99af6806a35a2c57f2b4b6
Kelly Munro <kelly.munro@powershop.co.nz>
2016-09-15 12:41:57 +1200

    Create a cat

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -4,4 +4,21 @@ class CatsController < ApplicationController
   def index
     @cats = current_user.cats.order(:name)
   end
+
+  def new
+    @cat = Cat.new
+  end
+
+  def create
+    puts params
+    @cat = Cat.create!(cat_params.merge(user: current_user))
+
+    redirect_to @cat
+  end
+
+  private
+
+  def cat_params
+    params.require(:cat).permit(:name, :registration_number, :breed_id, :coat_colour_id)
+  end
 end
